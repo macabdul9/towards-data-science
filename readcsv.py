d871b9eff40f2a91f2cb3825415e0b859e00c3cc
macabdul9 <abdulwaheed1513@gmail.com>
2019-04-17 17:05:12 +0530

    pycharm commit

--- readcsv.py
+++ readcsv.py
@@ -3,8 +3,8 @@ import pandas as pd
 # read csv file
 # a data frame object
 
-df = pd.read_csv('breast-cancer-wisconsin-data/data.csv')
+df = pd.read_csv('breast-cancer-wisconsin-data/data.csv', skiprows=1)
 
 # print(df)
 # to_print() to make csv content printing friendly !
-print(df.to_string())
+print(df.head(5).to_string())
